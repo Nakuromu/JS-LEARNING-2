8be001bd31d8dcb3e5da340b2a9fb3125315bb9e
Nakuromu <102924208+Nakuromu@users.noreply.github.com>
2024-08-25 20:27:26 +0000

    Deploying to gh-pages from @ Nakuromu/JS-LEARNING-2@572156286f35959a7ce7fe395a50f5607cd422a8 🚀

---
+++ 01-color-switcher.9cd87c55.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let d;e.setAttribute("disabled",""),t.addEventListener("click",(function(){t.setAttribute("disabled",""),e.removeAttribute("disabled"),d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;r.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){e.setAttribute("disabled",""),t.removeAttribute("disabled"),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.9cd87c55.js.map
---
+++ leson9/promises.5a93f06d.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=promises.5a93f06d.js.map
